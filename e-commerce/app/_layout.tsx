7fcd4fb0ae37af45db0adc9b0166f7fb1a4849d3
Sahaj Arya <sahajarya@Sahajs-MacBook-Air.local>
2024-10-19 22:10:00 +0530

    router

---
+++ e-commerce/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const RootNav = () => {
+  return <Tabs />;
+};
+
+export default RootNav;
+
+const styles = StyleSheet.create({});
